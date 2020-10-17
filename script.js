eeae386fb52ea5b6b8236081dfbc6bbbc79c1276
Melanie Richards <mr@melanie-richards.com>
2020-10-17 15:20:15 -0700

    Add setting: voices (fixes #2)

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 /*
  *  =============================================
+ *  GLOBALS
+ *  =============================================
+ */
+
+const voicesEl = document.getElementById('voices');
+const settingValueVoice = localStorage.getItem('setting-voice');
+
+/*
+ *  =============================================
  *  SOUNDBOARD SETUP
  *  =============================================
  */
@@ -55,6 +64,64 @@ const createButtons = function(stringFile) {
 const playSound = function(e) {
   const speechString = e.target.getAttribute('data-speech');
   const speechify = new SpeechSynthesisUtterance(speechString);
+  const voiceOptions = window.speechSynthesis.getVoices();
+  const selectedVoice = voicesEl.selectedOptions[0].getAttribute('data-name');
+
+  for (let i = 0; i < voiceOptions.length; i++) {
+    if (voiceOptions[i].name === selectedVoice) {
+      speechify.voice = voiceOptions[i];
+    }
+  }
+
   speechSynthesis.speak(speechify);
-  console.log('test');
 };
+
+/*
+ *  =============================================
+ *  SETTINGS
+ *  =============================================
+ */
+
+/*
+ *  SETTING: VOICE
+ *  ---------------------------------------------
+ */
+
+// Present voice options
+const listVoiceOptions = function() {
+  const voiceOptions = window.speechSynthesis.getVoices();
+
+  for (let i = 0; i < voiceOptions.length; i++) {
+    const option = document.createElement('option');
+
+    if (voiceOptions[i].default) {
+      option.textContent += 'Default voice';
+    } else {
+      option.textContent = voiceOptions[i].name + ' (' + voiceOptions[i].lang + ')';
+    }
+
+    option.setAttribute('data-lang', voiceOptions[i].lang);
+    option.setAttribute('data-name', voiceOptions[i].name);
+    if (settingValueVoice && settingValueVoice === voiceOptions[i].lang) {
+      option.setAttribute('selected', 'true');
+    }
+    voicesEl.appendChild(option);
+  }
+};
+
+listVoiceOptions();
+
+// Chromium needs onvoiceschanged to fire in order to populate; avoid re-populating
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  if (voicesEl.options.length === 0) {
+    speechSynthesis.onvoiceschanged = listVoiceOptions;
+  }
+}
+
+// Store setting: voice
+const storeVoiceSetting = function() {
+  const selectedVoice = voicesEl.selectedOptions[0].getAttribute('data-lang');
+  localStorage.setItem('setting-voice', selectedVoice);
+};
+
+voicesEl.addEventListener('change', storeVoiceSetting);
